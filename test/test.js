c36a68c05b76834e859257f3b22aa5814045d27f
caamillo <mcamillolud@gmail.com>
2023-10-31 10:36:38 +0100

    fix: adding sanitizing methods + fixed isNumber function

--- test/test.js
+++ test/test.js
@@ -3,10 +3,13 @@ const fs = require('fs')
 
 // Example Zip
 
-const exampleName = 'test4'
-const example = fs.readFileSync(`./examples/unzipped/${ exampleName }.min.json`, 'utf-8')
-const zipped = zip(example)
-fs.writeFileSync(`./examples/zipped/${ exampleName }.cz`, zipped.raw)
+// const exampleName = 'test4'
+// const example = fs.readFileSync(`./examples/unzipped/${ exampleName }.min.json`, 'utf-8')
+const zipped = zip('ciao 123 come va iop sono un ragazzo molto brvo ciao , cdavvero ciao davvero! dico amico!')
+const unzipped = unzip(zipped.raw, '', true)
+console.log(zipped.raw)
+console.log(unzipped)
+// fs.writeFileSync(`./examples/zipped/${ exampleName }.cz`, zipped.raw)
 
 
 /*
